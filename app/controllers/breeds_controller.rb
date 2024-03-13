40bc9ba6c4c31bf293f21743ab5166666dd76ea6
Tingting Wei <twei3@academic.rrc.ca>
2024-03-13 17:18:07 -0500

    Configure routes pointing to breeds controller actions and edit index.html.erb in breeds

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,4 +1,5 @@
 class BreedsController < ApplicationController
   def index
+    @breeds = Breed.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'breeds', to: 'breeds#index'
 end

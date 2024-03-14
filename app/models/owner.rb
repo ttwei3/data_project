fe9304ca70cf71022ff2d28f4267052dd4d47367
Tingting Wei <twei3@academic.rrc.ca>
2024-03-13 21:04:43 -0500

    create geocoder.rb file and Ensure geocoding is triggered when saving records

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,8 +1,8 @@
 class Owner < ApplicationRecord
   has_many :dogs
 
-  validates :owner_city, :owner_province, length: { minimum: 2, too_short: "must have at least 2 characters" }, allow_blank: true
-  validates :owner_postal_code, format: { with: /\A[0-9A-Za-z-]+\z/, message: "invalid format" }, allow_blank: true
+  geocoded_by :full_address
+  after_validation :geocode, if: ->(obj) { obj.full_address.present? && (obj.latitude.nil? || obj.longitude.nil?) }
 
   def full_address
     [owner_address, owner_city, owner_province, owner_postal_code, "Canada"].compact.join(', ')
---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,6 @@
+# config/initializers/geocoder.rb
+Geocoder.configure(
+  lookup: :google,
+  api_key: 'AIzaSyD4SsS7L7lBrQtih7D2fXkuisfG-d37HlU',
+  timeout: 5
+)

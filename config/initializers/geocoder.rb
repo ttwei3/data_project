f9bfc2cac2404e6dfcad960c4203744bd14749ca
Tingting Wei <twei3@academic.rrc.ca>
2024-03-13 21:05:55 -0500

    create geocoder.rb file and Ensure geocoding is triggered when saving records

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,9 @@
 # config/initializers/geocoder.rb
 Geocoder.configure(
+  #Use Google's geocoding service
   lookup: :google,
+  #api key
   api_key: 'AIzaSyD4SsS7L7lBrQtih7D2fXkuisfG-d37HlU',
+  #Set request timeout (seconds)
   timeout: 5
 )

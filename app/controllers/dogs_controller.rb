513ef20e7c947bba5004546ddb1278060f97b2cb
Tingting Wei <twei3@academic.rrc.ca>
2024-03-14 21:54:29 -0500

    add search funtion in dogs page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,12 @@
 class DogsController < ApplicationController
   def index
-    @dogs = Dog.page(params[:page]).per(10)
+    @dogs = if params[:breed_name].present?
+              Dog.joins(:breed).where('breeds.breed_name LIKE ?', "%#{params[:breed_name]}%").page(params[:page]).per(10)
+            else
+              Dog.page(params[:page]).per(10)
+            end
   end
+
 
   def show
     @dog = Dog.find(params[:id])

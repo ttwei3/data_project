1f40756819935f5983b4f6356d60954c86f2b69a
Tingting Wei <twei3@academic.rrc.ca>
2024-03-14 22:15:09 -0500

    user can search data by using dropdown

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,12 +1,21 @@
 class DogsController < ApplicationController
   def index
-    @dogs = if params[:breed_name].present?
-              Dog.joins(:breed).where('breeds.breed_name LIKE ?', "%#{params[:breed_name]}%").page(params[:page]).per(10)
-            else
-              Dog.page(params[:page]).per(10)
-            end
-  end
+    @dogs = Dog.all
 
+    if params[:breed_name].present?
+      @dogs = @dogs.joins(:breed).where('breeds.breed_name LIKE ?', "%#{params[:breed_name]}%")
+    end
+
+    if params[:has_owner].present?
+      if params[:has_owner] == 'true'
+        @dogs = @dogs.joins(:owner)
+      elsif params[:has_owner] == 'false'
+        @dogs = @dogs.where(owner: nil)
+      end
+    end
+
+    @dogs = @dogs.page(params[:page]).per(10)
+  end
 
   def show
     @dog = Dog.find(params[:id])

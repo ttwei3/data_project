70782bdf360ccb0509f6f438377d611d495a42eb
Tingting Wei <twei3@academic.rrc.ca>
2024-03-14 23:45:19 -0500

    add validations in owners model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,12 @@
 class Owner < ApplicationRecord
   has_many :dogs
 
+  validates :owner_name, presence: true
+  validates :owner_address, presence: true
+  validates :owner_city, presence: true
+  validates :owner_province, presence: true
+  validates :owner_postal_code, presence: true
+
   geocoded_by :full_address
   after_validation :geocode, if: ->(obj) { obj.full_address.present? && (obj.latitude.nil? || obj.longitude.nil?) }
 
